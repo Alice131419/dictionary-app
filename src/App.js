6a7429c8f8e35b97297171f42c9f18430fe63156
Alice131419 <manuscarlet@yahoo.com>
2024-11-25 18:24:10 +0200

    Added footer links

--- src/App.js
+++ src/App.js
@@ -13,7 +13,30 @@ export default function App() {
         </main>
         <footer className="App-footer">
           <small>
-            Coded by <em>Alice Petcu</em>
+            Coded by{" "}
+            <a href="https://github.com/" target="_blank" rel="noreferrer">
+              {" "}
+              <em>Alice Petcu</em>{" "}
+            </a>
+            ,{" "}
+            <a
+              href="https://dictionary-app-by-alice-petcu.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {" "}
+              open sourced on GitHub
+            </a>{" "}
+            and
+            <a
+              href="https://app.netlify.com/sites/dictionary-app-by-alice-petcu/overview"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {" "}
+              hosted on Netlify
+            </a>
+            .
           </small>
         </footer>
       </div>
